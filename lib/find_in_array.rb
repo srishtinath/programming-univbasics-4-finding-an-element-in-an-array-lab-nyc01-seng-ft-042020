caf939b129b7647ca0b1668929e730ede7c7e575
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 01:52:40 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   if array.include?(value_to_find)
-    return array.index_at(value_to_find)
+    return array.index(value_to_find)
   else
     return nil
   end
